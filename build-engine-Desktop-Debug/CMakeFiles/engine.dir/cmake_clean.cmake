ab44e2df4469b877c671387337627e4a7101f718
LaterNerds <theo.ls.raymond@icloud.com>
2024-05-16 06:30:01 -0500

    first commit

---
+++ build-engine-Desktop-Debug/CMakeFiles/engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine.dir/main.c.o"
+  "CMakeFiles/engine.dir/main.c.o.d"
+  "engine"
+  "engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
